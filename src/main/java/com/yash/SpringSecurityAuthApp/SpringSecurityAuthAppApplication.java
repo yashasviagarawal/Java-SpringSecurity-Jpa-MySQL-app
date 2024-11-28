949b08dfe552d42ddf8dd80872ec678ac237962e
Yashasvi Agarawal <yash21011999@gmail.com>
2024-11-28 23:36:21 +0530

    first commit 28/11/24

---
+++ src/main/java/com/yash/SpringSecurityAuthApp/SpringSecurityAuthAppApplication.java
@@ -0,0 +1,13 @@
+package com.yash.SpringSecurityAuthApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yash/SpringSecurityAuthApp/SpringSecurityAuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.SpringSecurityAuthApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
